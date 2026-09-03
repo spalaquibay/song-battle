b74216de56805edc32f097efda34760bef98be83
spalaquibay <spalaquibay@gmail.com>
2026-09-03 14:18:13 -0500

    Agregar creación de partidas y sala

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+import string
 
 app = Flask(__name__)
 
@@ -6,6 +8,28 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+
+@app.route("/crear-partida", methods=["GET", "POST"])
+def crear_partida():
+
+    if request.method == "POST":
+
+        categoria = request.form["categoria"]
+        preguntas = request.form["preguntas"]
+
+        codigo = ''.join(
+            random.choices(string.ascii_uppercase + string.digits, k=4)
+        )
+
+        return render_template(
+            "sala.html",
+            codigo=codigo,
+            categoria=categoria,
+            preguntas=preguntas
+        )
+
+    return render_template("crear_partida.html")
 
 
 if __name__ == "__main__":
